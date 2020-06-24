642eee3a52113c3646001e83a54ef458cfd4c0e0
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-06-24 15:26:57 -0300

    layouts

---
+++ src/main/java/com/eventoapp/eventoapp/SobreController.java
@@ -0,0 +1,12 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SobreController {
+	@RequestMapping("/sobre")
+	public String sobre() {
+		return "sobre";
+	}
+}
